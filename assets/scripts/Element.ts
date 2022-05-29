bf8823b86b9de728d16eb865ceff5dc047d63141
LunaticHS <78539678+LunaticHS@users.noreply.github.com>
2022-05-30 02:26:03 +0800

    加了点难度

--- assets/scripts/Element.ts
+++ assets/scripts/Element.ts
@@ -40,10 +40,22 @@ export class Element extends Component {
             table.push(new Vec3(GRASS,   GROUND,   BEAR));
             table.push(new Vec3(ICE,   LEMON,   ICECREAM));
             table.push(new Vec3(HANDSSOME,   CLOUD,   SUSHI));
-            table.push(new Vec3(ICECREAM,   SUSHI,   RAINBOW));
             table.push(new Vec3(WATER,   FIRE,   CLOUD));
             table.push(new Vec3(WATER,   WIND,   ICE));
             table.push(new Vec3(GROUND,   FIRE,   METAL));
+            table.push(new Vec3(ICECREAM,   SUSHI,   RAINBOW));
+            
+            table.push(new Vec3(HANDSSOME,   SUSHI,   HANDSSOME));
+            table.push(new Vec3(HANDSSOME,   ICECREAM,   HANDSSOME));
+            table.push(new Vec3(HANDSSOME,   LEMON,   HANDSSOME));
+
+            table.push(new Vec3(BEAR,   GRASS,   BEAR));
+            table.push(new Vec3(BEAR,   ICECREAM,   BEAR));
+            table.push(new Vec3(BEAR,   LEMON,   BEAR));
+            table.push(new Vec3(BEAR,   SUSHI,   BEAR));
+            
+            table.push(new Vec3(FIRE, GRASS, FIRE));
+            table.push(new Vec3(METAL, THUNDER, METAL));
 
             Element.table = table;
         }
