00ac1615a051ae2fd92baddb0dc0038caadc5814
String Max <mr.tens@foxmail.com>
2022-05-29 17:04:31 +0800

    Add fusion-table
    Now it will follow the table
    and get a new slime!

--- assets/scripts/Element.ts
+++ assets/scripts/Element.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Contact2DType, IPhysics2DContact, tween, Collider2D } from 'cc';
+import { _decorator, Component, Contact2DType, IPhysics2DContact, tween, Collider2D, Enum, Vec2, Vec3 } from 'cc';
 import { Bucket } from './Bucket';
 const { ccclass, property } = _decorator;
 
@@ -7,7 +7,25 @@ export class Element extends Component {
     @property
     elemNumber: number = 0;
 
+    static table: Vec3[] = null;
+
     onLoad() {
+        if(Element.table == null) {
+            let FIRE = 0,
+                WATER = 1,
+                GRASS = 2,
+                GROUND = 3,
+                THUNDER = 4,
+                ICE = 5,
+                WIND = 6;
+            var table = [];
+            table.push(new Vec3(WATER,  GROUND, GRASS));
+            table.push(new Vec3(WATER,  WIND,   ICE));
+            table.push(new Vec3(FIRE,   WIND,   THUNDER));
+
+            Element.table = table;
+        }
+
         let collider = this.getComponent(Collider2D);
         collider.on(Contact2DType.BEGIN_CONTACT, this.onBeginContact, this);
     }
@@ -33,10 +51,20 @@ export class Element extends Component {
             }
             let selfNumber = selfCollider.node.getComponent(Element).elemNumber;
             let otherNumber = otherCollider.node.getComponent(Element).elemNumber;
-            if(selfNumber != otherNumber || selfNumber + 1 == Bucket.instance.elemSprites.length) {
-                return;
+            var targetNumber = -1;
+
+            for(var i = 0; i < Element.table.length; i++) {
+                var rule = Element.table[i];
+                if(rule.x == selfNumber && rule.y == otherNumber ||
+                        rule.y == selfNumber && rule.x == otherNumber) {
+                    targetNumber = rule.z;
+                    break;
+                }
             }
 
+            if(targetNumber < 0) {
+                return;
+            }
 
             let newPos = otherCollider.node.getPosition();
             //otherCollider.node.getComponent(Collider2D).radius = 0;
@@ -50,7 +78,7 @@ export class Element extends Component {
                     position: newPos,
                 }
             ).call(function () {
-                Bucket.instance.createLevelUpElem(selfNumber + 1, newPos);
+                Bucket.instance.createLevelUpElem(targetNumber, newPos);
                 selfCollider.node.active = false;
                 otherCollider.node.active = false;
                 selfCollider.node.destroy();
